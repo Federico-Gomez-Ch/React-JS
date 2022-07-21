c040194f67fd9cf5198524a54f70a34021612397
federicogomez016 <federicogomez016@outlook.com>
2022-07-20 23:37:28 -0300

    m

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
-/* import Counter from './components/Navbar/Counter/Counter'; */
+import Counter from './components/Navbar/Counter/Counter';
 import ItemListContainer from './components/Navbar/ItemListContainer/ItemListContainer';
 
 function App() {
+
+  const [show, setShow] = useState(true)
   return (
     <div className="App">
         <Navbar/>
-{/*         <Counter/> */}
+        <Counter show={show} stock={10} initial={1}/>
         <ItemListContainer greeting="bienvenidx"/>
         
     </div>
--- src/components/Navbar/Counter/Counter.js
+++ src/components/Navbar/Counter/Counter.js
@@ -1,20 +1,29 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
-const Counter = () => {
-    /* const state = useState (0) */
+const Counter = ({show,stock,initial}) => {
+ 
     const [count, setCount] = useState(0)
+    
+useEffect (() =>{
+    console.log('function callback useEffect')
 
-
+    return()=> console.log('el componente va a desmontarse')
+},[])
 const decrement = () => {
    /*  state[1](state[0] - 1) */
-   setCount(count - 1);
+   if(count > initial){
+    setCount(count - 1);
+   }
+   
 }
 
 const increment = () => {
    /*  state[1](state[0] + 1) */
-   setCount(count + 1);
+   if(count < stock){
+    setCount(count + 1)
+   }
 }
-
+console.log('va a renderizar')
 return (
     <div>
         <h1>{count}</h1>
